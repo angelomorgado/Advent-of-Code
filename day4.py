aa36c0942cce63a9644a9fa8e45ad1cc12dffd92
angelomorgado <angelo_morgado@hotmail.com>
2021-12-07 02:06:30 +0000

    day 4 ex1 done

---
+++ day4.py
@@ -0,0 +1,75 @@
+import numpy as np
+import itertools
+
+data = open("input_day4.txt","r")
+#data = open("test.txt","r")
+
+#Checks if the dictionary wins
+def checkWin(d):
+    #turn dict to list
+    l = list(d.values())
+    
+    matrix = np.array(l).reshape(5,5)
+        
+    #Check win conditions
+    for i in range(5):
+        #Check lines
+        if matrix[i,:].all() == 1:
+            return True
+        
+        #Check columns
+        if matrix[:,i].all() == 1:
+            return True
+    return False    
+
+
+def sumAllZeroes(d):
+    #Initiates coutn
+    count = 0
+    #sums every that has 0
+    for i in d:
+        if d.get(i) == 0:
+            count += i
+    return count
+    
+
+def ex1():
+    #Reads the first line of results and stores it in a list (Strip gets rid of the \n)
+    results = data.readline().strip().split(',')
+    print(results)
+    
+    #gets rid of annoying space
+    space = data.readline()
+    
+    #Reads the remaining of the file and stores it in a list of matrices
+    dictList = []
+    for line in data:
+        auxDict = {}
+        for i in range(5):
+            for f in line.strip().split(' '):
+                auxDict[int(f)] = 0
+                
+            line = data.readline()
+        dictList.append(auxDict)
+    
+    
+    #Goes through the list and adds the checkmark
+    previousNum = -1
+    
+    for i in results:
+        for d in dictList:
+            #If the dictionary wins
+            if checkWin(d):
+                sum = sumAllZeroes(d)
+                print(sum ,' * ', previousNum ,'= ', sum*int(previousNum))
+                return
+            
+            #If the key exists in the dictionary
+            if int(i) in d:
+                d[int(i)] = 1
+                
+        previousNum = i
+    
+                
+
+ex1()
