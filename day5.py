08e51b1577fbd98404d09438e8c386577cc62c2b
angelomorgado <angelo_morgado@hotmail.com>
2021-12-08 23:46:38 +0000

    day 5 ex1 and almost the second

---
+++ day5.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+data = open("test.txt","r")
+#data = open("input_day5.txt","r")
+
+
+def addToMatrix(lineMatrix, p):
+    
+    #So that the for executes flawlessly
+    if p[0] > p[2]:
+        controlX = -1
+    else:
+        controlX = 1
+    
+    if p[1] > p[3]:
+        controlY = -1
+    else:
+        controlY = 1
+    
+        
+        
+    #Verify if it's only vertical or horizontal
+    #If it's vertical (y)
+    if p[0] == p[2]:
+        for i in range(p[1], p[3] + controlY,controlY):
+            lineMatrix[i][p[0]] += 1
+        return lineMatrix
+    
+    #If it's horizontal
+    if p[1] == p[3]:
+        for i in range(p[0], p[2] + controlX,controlX):
+            lineMatrix[p[1]][i] += 1
+        return lineMatrix
+    
+    #If it's diagonal (exercise 2 comment if you want part 1)
+    #directly porpotional
+    if p[0] - p[1] == p[2] - p[3]:
+        auxMatrix = np.zeros(100).reshape(10,10) 
+        
+        if p[0] < p[2]:
+            controlDx = 1
+            controlDy = -1
+        else:
+            controlDx = -1
+            controlDy = 1
+        
+        x = p[0]
+        y = p[1]
+        for i in range((p[2] - p[0]) * controlDx + 1):
+            lineMatrix[y][x] += 1
+            auxMatrix[y][x] += 1
+            x += controlDx
+            y += controlDy
+        print("direct: (",p[0],",",p[1],") (",p[2],",",p[3],")")
+        print(auxMatrix)        
+        return lineMatrix
+
+    #invertly porpotional
+    if p[0] + p[1] == p[2] + p[3]:
+        auxMatrix = np.zeros(100).reshape(10,10) 
+        
+        if p[0] < p[2]:
+            controlDx = 1
+            controlDy = 1
+        else:
+            controlDx = -1
+            controlDy = -1
+        
+        x = p[0]
+        y = p[1]
+        for i in range((p[2] - p[0]) * controlDx + 1):
+            lineMatrix[y][x] += 1
+            auxMatrix[y][x] += 1
+            x += controlDx
+            y += controlDy
+        print("Invert: (",p[0],",",p[1],") (",p[2],",",p[3],")")
+        print(auxMatrix,"\n")
+        return lineMatrix
+    return lineMatrix
+    
+
+def ex1():
+    
+    lineList = []
+    lineMatrix = np.zeros(100).reshape(10,10) 
+    for l in data:
+        #Organize the data
+        x1y1,x2y2 = l.strip().split(" -> ")
+        x1,y1 = x1y1.split(",")
+        x2,y2 = x2y2.split(",")
+        #lineList.append([int(x1),int(y1),int(x2),int(y2)])
+        lineMatrix = addToMatrix(lineMatrix, [int(x1),int(y1),int(x2),int(y2)])
+    
+    
+    print(lineMatrix)  
+    
+    count = 0  
+    for i in lineMatrix:
+        for f in i:
+            if f > 1:
+                count += 1
+    
+    print('count = ',count)
+
+
+ex1()
